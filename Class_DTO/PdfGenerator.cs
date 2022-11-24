7221a5ea57415c5633b96c4880683c68537da59f
Danielwith <daniel.orosco.sc2@hotmail.com>
2022-11-23 22:17:56 -0500

    EmailSender Implementado

--- Class_DTO/PdfGenerator.cs
+++ Class_DTO/PdfGenerator.cs
@@ -18,21 +18,22 @@ namespace ShopBook.Class_DTO
     {
         private shopbookEntities db = new shopbookEntities();
         private decimal? total=0;
-        public void generate(List<ShoppingCart> compras)
+        public void generate(List<ShoppingCart> compras, string path, ClientData cliente)
         {
-            MemoryStream ms = new MemoryStream();
             // Must have write permissions to the path folder
-            PdfWriter writer = new PdfWriter("C:/Users/The/Desktop/API/Demo.pdf");
+            PdfWriter writer = new PdfWriter(path);
             PdfDocument pdf = new PdfDocument(writer);
             Document document = new Document(pdf);
+
             Paragraph header = new Paragraph("COMPROBANTE DE PAGO")
                .SetTextAlignment(TextAlignment.CENTER)
-               .SetFontSize(20);
+               .SetFontSize(20)
+               .SetBold();
             document.Add(header);
 
             Paragraph nombre = new Paragraph("Shopbook S.A")
                .SetTextAlignment(TextAlignment.LEFT)
-               .SetFontSize(10)
+               .SetFontSize(14)
                .SetBold();
             document.Add(nombre);
 
@@ -60,13 +61,50 @@ namespace ShopBook.Class_DTO
                .SetFontSize(10);
             document.Add(fecha);
 
+            // Line separator
+            document.Add(ls);
+            Paragraph dataCli = new Paragraph("Info del Cliente:")
+               .SetTextAlignment(TextAlignment.LEFT)
+               .SetBold()
+               .SetUnderline()
+               .SetFontSize(14);
+            document.Add(dataCli);
+
+            Paragraph email = new Paragraph("Correo Electronico: " + cliente.email)
+               .SetTextAlignment(TextAlignment.LEFT)
+               .SetFontSize(10);
+            document.Add(email);
+
+            Paragraph nomape = new Paragraph("Nombres y Apellidos: " + cliente.name +" "+cliente.apellido)
+               .SetTextAlignment(TextAlignment.LEFT)
+               .SetFontSize(10);
+            document.Add(nomape);
+
+            Paragraph docNum = new Paragraph("DNI / RUC: "+cliente.docNum)
+               .SetTextAlignment(TextAlignment.LEFT)
+               .SetFontSize(10);
+            document.Add(docNum);
+
+            Paragraph phone = new Paragraph("Telefono: " + cliente.phone)
+               .SetTextAlignment(TextAlignment.LEFT)
+               .SetFontSize(10);
+            document.Add(phone);
+
+            Paragraph direccion = new Paragraph("Dirección: " + cliente.direccion)
+               .SetTextAlignment(TextAlignment.LEFT)
+               .SetFontSize(10);
+            document.Add(direccion);
+
+            document.Add(ls);
+
             // New line
             Paragraph newline = new Paragraph(new Text("\n"));
-            document.Add(newline);
 
             Paragraph resumen = new Paragraph("Resumen de Compra:")
                        .SetTextAlignment(TextAlignment.LEFT)
-                       .SetFontSize(10);
+                       .SetFontSize(14)
+                       .SetBold()
+                       .SetUnderline();
             document.Add(resumen);
 
             Table table = new Table(4, true);
@@ -127,7 +165,6 @@ namespace ShopBook.Class_DTO
             document.Add(newline);
             document.Add(totalCompra);
 
-            
             document.Close();
         }
     }
